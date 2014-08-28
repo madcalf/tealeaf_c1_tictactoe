c9402027dc57c8b60d56a1eb2974545e0c901ed7
madcalf <ddd@DsMacBookPro-4.local>
2014-08-28 17:36:47 -0400

    Computer is smarter now; still no grid drawing

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,19 +1,16 @@
 # Course 1. Lesson 1. Tic Tac Toe
 require 'pry'
-require 'set'
 
-# sample game array with 1, 5, 9 as x
-# game_array: ["x", ".", ".", ".", "x", ".", "o", ".", "x"]
 # these are patterns of array indexes into game_array that yield wins
 WIN_PATTERNS =  [ [0, 1, 2], [3, 4, 5], [6, 7, 8], 
                   [0, 3, 6], [1, 4, 7], [2, 5, 8], 
                   [0, 4, 8], [2, 4, 6]
                 ]
 EMPTY = "."
-PLAYER = {name: "Player", char: "x"}
-COMPUTER = {name: "Computer", char: "o"}  
+PLAYER = {name: "Player", marker: "x"}
+COMPUTER = {name: "Computer", marker: "o"}  
 
- 
+
 def print_array_as_grid(array)
   # just for debugging to visualize the board status
   # until we're actually drawing it
@@ -22,20 +19,26 @@ def print_array_as_grid(array)
   puts "#{array[6..8]}"
 end 
 
-# make this take multiple optional args
-# so we can add multiple x's for testing and such
-def set_player_choice(arr, pos)
-  arr[pos - 1] = "x"
+# computer needs to do things a bit differently
+def computer_move(game_arr)
+  index = look_for_win(game_arr)
+  if !index 
+    index = look_for_block(game_arr)
+  end
+  
+  if index
+   pos = index + 1
+  else
+    # no win or block, so go random
+    pos = find_empty_pos(game_arr)
+  end
+  set_marker(game_arr, COMPUTER, pos)  
 end
 
-def set_computer_choice(arr, pos)
-  arr[pos - 1] = "o"
-end
-
-def set_choice(game_arr, which_player, pos)
+def set_marker(game_arr, which_player, pos)
   index = pos - 1
   if game_arr[index] == EMPTY
-    game_arr[pos - 1] = which_player[:char]
+    game_arr[pos - 1] = which_player[:marker]
     puts "#{which_player[:name]} chose #{pos}"
     return true
   else
@@ -49,20 +52,18 @@ def find_empty_pos(game_arr)
 end
 
 def test_for_win(game_arr, which_player)
-  puts "test_for_win()"
   # loop thru the winning index combos
   # for each combo, see if there's x's or o's in all 3 indexes of game_arr
   is_win = false
   WIN_PATTERNS.each_with_index do |list_of_indexes, i| 
     value_list = game_arr.values_at(*list_of_indexes)
-    is_win = value_list.all? { |val| val == which_player[:char]}
+    is_win = value_list.all? { |val| val == which_player[:marker]}
     break if is_win
   end
   return is_win
 end
 
 def test_for_end(game_arr)
-  puts "test_for_end"
   if test_for_win(game_arr, PLAYER)
     puts "#{PLAYER[:name].upcase} WINS!"
     true
@@ -71,38 +72,57 @@ def test_for_end(game_arr)
     true
   elsif !find_empty_pos(game_arr)
     puts "TIE GAME!"
+    true
   else
     false
-  end    
+  end
 end
 
-# test for any 2 x's or o's in any of the win sequences
-# e.g. x.x | xx. | .xx | .oo | o.o | oo.
-# order doesn't matter, just get the space that's empty if there's 2!
-def test_for_block(game_arr, player, player_to_block)     
+# only computer does this
+# look thru the winning sequence indexes for 2 o's and EMPTY
+# the EMPTY is where computer can win!
+def look_for_win(game_arr)
+  WIN_PATTERNS.each_with_index do |indexes, i| 
+    values = game_arr.values_at(*indexes)
+    can_win = values.select { |item| item == "o"}.size == 2
+ 
+    # now get the empty index they need to win
+    if can_win
+      # find the index that's empty in the values list
+      index1 = values.find_index { |item| item == EMPTY }
+      # use that index to query indexes for the main index in game_array to block
+      if (index1)
+        # get the index into the game_array
+        index2 = indexes[index1]
+        return index2 
+      end
+    end
+  end 
+  return nil
+end
+
+# only computer does this
+# look thru the winning sequence indexes for 2 x's and EMPTY
+# the EMPTY is where computer needs to block player from winning
+def look_for_block(game_arr)     
   # loop thru the winning index combos and see if we find
   # for each combo, see if there's 2 x's or o's in a row indexes of game_arr
-  char = player_to_block[:char]
-  WIN_PATTERNS.each_with_index do |list_of_indexes, i| 
-    value_list = game_arr.values_at(*list_of_indexes)
-    # puts "TEST: #{value_list.find_index { |item| item != char }}"
-    # puts "need_to_block: #{need_to_block}"
-    need_to_block = value_list.select { |item| item == char}.size == 2
+  WIN_PATTERNS.each_with_index do |indexes, i| 
+    values = game_arr.values_at(*indexes)
+    can_block = values.select { |item| item == "x"}.size == 2
  
-    # now get the index they need to block
-    if need_to_block
-      # find the index that's not char in the local 3 item value_list
-      local_index_to_block = value_list.find_index { |item| item != char }
-      puts "list_of_indexes: #{list_of_indexes}  local_index_to_block: #{local_index_to_block} } "
-      # use that index to query list_of_indexes for the main index in game_array to block
-      if (local_index_to_block)
-        main_index_to_block = list_of_indexes[local_index_to_block]
-        puts "#{player[:name]} needs to block at #{main_index_to_block} (position #{main_index_to_block + 1})]  #{value_list}" 
+    # now get the empty index they need to block
+    if can_block
+      # find the index that's empty in the local values list
+      index1 = values.find_index { |item| item == EMPTY }
+      # get the index into the game_array
+      if (index1)
+        index2 = indexes[index1]
+        return index2
       end
     end
   end
-    # is_win = value_list.all? { |val| val == player[:char]}
-    # puts "#{player[:name]} WINS! value_list: #{value_list}" if is_win
+  return nil
 end
 
 # updates screen with our pseudo gameboard
@@ -115,25 +135,12 @@ def update(game_arr, msg = "\n")
   puts msg
 end
 
-def new_game(game_array)
-  game_array = Array.new(9, '.')
-end
-
 # ------ END METHODS ------ 
 
 system 'clear'
-game_array = Array.new(9, '.')
-
-
-# testing values
-# set_choice(game_array, player, 1)
-# set_choice(game_array, player, 4)
-# set_choice(game_array, player, 7)
-
-# set_choice(game_array, computer, 3)
-# set_choice(game_array, computer, 6)
-# set_choice(game_array, computer, 9)
-
+# sample game array with 1, 5, 9 as x
+# looks like this: ["x", ".", ".", ".", "x", ".", "o", ".", "x"]
+# game_array = Array.new(9, '.')
 
 # main program loop
 loop do  
@@ -148,23 +155,26 @@ loop do
     # player's move
     begin
       input = gets.chomp.downcase
-      break if set_choice(game_array, PLAYER, input.to_i)
+      break if set_marker(game_array, PLAYER, input.to_i)
       update(game_array, "Already taken! Pick another")
     end while true 
 
-    update(game_array, "" )
+    puts "Player's turn done. Updating with no message"
+    
+    update(game_array)
     if test_for_end(game_array)
       puts "Play again?"
       gets.chomp.downcase == 'y' ? break : exit
     end
-    
+ 
+     puts "Computer's turn starting. Updating with 'my turn'"   
     # computer's move
-    update(game_array, "My turn" )
-    pos = find_empty_pos(game_array)
-    puts "computer pos: #{pos}"
-    set_choice(game_array, COMPUTER, pos )
+    update(game_array, "Computer's turn" )
+    computer_move(game_array)
+  
+    puts "Computer's turn done. Updating with no message"
+   
     update(game_array)
-    
     if test_for_end(game_array)
       puts "Play again?"
       gets.chomp.downcase == 'y' ? break : exit
